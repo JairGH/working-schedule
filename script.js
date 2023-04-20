dacf9ba4e65f6df9e4ee68e16bbb81dfbb104bcc
Jair Gomez <jairherber505@gmail.com>
2023-04-20 12:35:52 -0700

    98% done

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var today = dayjs();
-$('#currentDate').text(today.format('MMM D, YYYY'));
-
 $(document).ready(function () {
+  var today = dayjs();
+  $("#currentDate").text(today.format("MMM D, YYYY"));
+
+  // im thinking off having this as the present and then make a if statement something like if (hourTime === hour time colorbackground.present)
+  // var hourTime = dayjs().format("h A");
+  // $("p").text(hourTime);
+
   $(".saveBtn").on("click", function () {
     var description = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
@@ -9,19 +13,46 @@ $(document).ready(function () {
     console.log(time);
 
     localStorage.setItem(time, description);
-  })
+  });
 });
 
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+// $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 
 $(".time-block").each(function () {
-  var now = dayjs().hour();
-  var calenderHour = parseInt($(this).parent().attr("id").split("-")[1]);
-  if (now < calenderHour) {
+  var now = dayjs().format("HH");
+  console.log($(this).attr("id").split("-"));
+  var calenderHour = parseInt($(this).attr("id").split("-")[1]);
+  if (now > calenderHour) {
     $(this).addClass("past");
-  } else if (now === calenderHour) {
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+  } else if (now == calenderHour) {
     $(this).addClass("present");
+    $(this).removeClass("future");
+    $(this).removeClass("past");
   } else {
     $(this).addClass("future");
+    $(this).removeClass("present");
+    $(this).removeClass("past");
   }
-})
+  console.log(now);
+});
+
+// var hourTime = dayjs();
+// $("hour-11").text(hourTime.format("h"));
+var hourCounter = 3;
+for (let i = 5; i < 18; i++) {
+  var currentTimeBlock = `
+  <div id="hour-${i}" class="row time-block future">
+        <div class="col-2 col-md-1 hour text-center py-3">${hourCounter}PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" id="saveBtn">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
+  `;
+  $(".container-lg").append(currentTimeBlock);
+  hourCounter++;
+}
+
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
